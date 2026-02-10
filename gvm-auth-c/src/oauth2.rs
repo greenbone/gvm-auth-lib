a5aae293d251852a59f7abf8747355f56157c92f
ozgen <ozgn45@gmail.com>
2026-02-10 13:51:13 +0100

    Change certs from AGPL to GPL

--- gvm-auth-c/src/oauth2.rs
+++ gvm-auth-c/src/oauth2.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Greenbone AG
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 use crate::set_err;
 use crate::strings::rs_string_to_c_ptr;
--- gvm-auth/src/clock.rs
+++ gvm-auth/src/clock.rs
@@ -1,6 +1,6 @@
 // SPDX-FileCopyrightText: 2026 Greenbone AG
 //
-// SPDX-License-Identifier: AGPL-3.0-or-later
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 use std::fmt::Debug;
 use std::sync::Arc;
